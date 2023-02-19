88178c7d2c2fc370220b9e2feffc6f7215fc10e2
gewal0815 <gerrywalter@hotmail.de>
2023-02-19 09:17:34 +0100

    Start1

---
+++ InteliAIClipboard/pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>My Clipboard App</h1>
+    <Clipboard />
+  </div>
+</template>
+
+<script>
+import Clipboard from '@/components/Clipboard';
+
+export default {
+  components: {
+    Clipboard
+  }
+};
+</script>
+
+<style>
+
+</style>
+
