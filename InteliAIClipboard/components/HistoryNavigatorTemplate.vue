dbfe1d497f2d18ff5aa5e962e0eab757e8e3fe4a
gewal0815 <gerrywalter@hotmail.de>
2023-02-20 04:33:03 +0100

    RefactoredMethodsand Template

---
+++ InteliAIClipboard/components/HistoryNavigatorTemplate.vue
@@ -0,0 +1,39 @@
+<template>
+
+    <div class="history-navigator">
+      <ul>
+        <li v-if="history.length === 0">There's no History content</li>
+        <li
+          v-else
+          v-for="(item, index) in history"
+          :key="item.id"
+          @click="selectItem(item)"
+        >
+          <span class="delete-item" @click.stop="deleteItem(index)">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/5974/5974771.png"
+              alt="Delete"
+            />
+          </span>
+          {{ item.content }}
+        </li>
+      </ul>
+    </div>
+  </template>
+  <script>
+  export default {
+    props: {
+      history: Array,
+      inputValue: String,
+    },
+    methods: {
+      deleteItem(index) {
+        this.$emit('delete-item', index);
+      },
+  
+      selectItem(item) {
+        this.$emit('select-item', item);
+      },
+    },
+  };
+  </script>
--- InteliAIClipboard/components/Main.vue
+++ InteliAIClipboard/components/Main.vue
@@ -1,64 +1,60 @@
 <template>
-    <div class="page-wrapper">
-      <div class="history-navigator">
-        <ul>
-          <li v-if="history.length === 0">There's no History content</li>
-          <li
-            v-else
-            v-for="(item, index) in history"
-            :key="item.id"
-            @click="selectItem(item)"
-          >
-            <span class="delete-item" @click.stop="deleteItem(index)">
-              <img
-                src="https://cdn-icons-png.flaticon.com/512/5974/5974771.png"
-                alt="Delete"
-              />
-            </span>
-            {{ item.content }}
-          </li>
-        </ul>
-      </div>
-      <div class="main-content">
-        <div class="main-content-show">
-          <!-- Input field and search button -->
-          <input
-            class="input"
-            type="text"
-            v-model="inputValue"
-            placeholder="Type here..."
-            @keydown.enter="addItem"
-          />
-          <i class="fas fa-search" @click="addItem"></i>
-  
-  
-        </div>
-      </div>
-      <div class="side-navigator">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus a velit
-          in est aliquet faucibus quis sit amet elit. Nunc et maximus eros. Mauris
-          nec metus vitae eros eleifend pulvinar vel eget leo.
-        </p>
+  <div class="page-wrapper">
+    <HistoryNavigatorTemplate
+      :history="history"
+      :input-value="inputValue"
+      @delete-item="deleteItem"
+      @select-item="selectItem"
+    />
+    <div class="main-content">
+      <div class="main-content-show">
+        <!-- Input field and search button -->
+        <input
+          class="input"
+          type="text"
+          v-model="inputValue"
+          placeholder="Type here..."
+          @keydown.enter="addItem"
+        />
+        <i class="fas fa-search" @click="addItem"></i>
       </div>
     </div>
-  </template>
-  
-  <script>
-  import HistoryNavigatorMethods from './HistoryNavigatorMethods.vue';
-  
-  export default {
-    mixins: [HistoryNavigatorMethods],
-    
-    data() {
-      return {
-        history: [],
-        inputValue: '',
-      };
+    <div class="side-navigator">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus a velit
+        in est aliquet faucibus quis sit amet elit. Nunc et maximus eros. Mauris
+        nec metus vitae eros eleifend pulvinar vel eget leo.
+      </p>
+    </div>
+  </div>
+</template>
+<script>
+import HistoryNavigatorMethods from './HistoryNavigatorMethods.vue';
+
+export default {
+  mixins: [HistoryNavigatorMethods],
+
+  data() {
+    return {
+      history: [],
+      inputValue: '',
+    };
+  },
+
+  methods: {
+
+
+    selectItem(item) {
+      console.log(item);
     },
-  };
-  </script>
+  },
+};
+</script>
 
 <style>
- @import "../utils/scss/Main.scss";
+@import '../utils/scss/Main.scss';
+</style>
+
+<style>
+@import '../utils/scss/Main.scss';
 </style>
