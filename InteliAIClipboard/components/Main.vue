a30776d6170c12c9cf36f5e87850f80f2791ef2c
gewal0815 <gerrywalter@hotmail.de>
2023-02-20 06:54:13 +0100

    test

--- InteliAIClipboard/components/Main.vue
+++ InteliAIClipboard/components/Main.vue
@@ -1,31 +1,36 @@
 <template>
-  <div class="page-wrapper">
-    <HistoryNavigatorTemplate
-      :history="history"
-      :input-value="inputValue"
-      @delete-item="deleteItem"
-      @select-item="selectItem"
-    />
-    <div class="main-content">
-      <div class="main-content-show">
-        <!-- Input field and search button -->
-        <input
-          class="input"
-          type="text"
-          v-model="inputValue"
-          placeholder="Type here..."
-          @keydown.enter="addItem"
-        />
-        <i class="fas fa-search" @click="addItem"></i>
+    <div class="page-wrapper">
+      <HistoryNavigatorTemplate
+        :history="history"
+        :input-value="inputValue"
+        @delete-item="deleteItem"
+        @select-item="selectItem"
+      />
+      <div class="main-content">
+        <div class="main-content-show">
+          <!-- Input field and search button -->
+          <input
+            class="input"
+            type="text"
+            v-model="inputValue"
+            placeholder="Type here..."
+            @keydown.enter="addItem"
+          />
+          <i class="fas fa-search" @click="addItem"></i>
+          <!-- New div with textarea -->
+          <div>
+            <textarea v-model="inputValue" class="text-area"></textarea>
+          </div>
+        </div>
+      </div>
+      <div class="side-navigator">
+        <p>
+          
+        </p>
       </div>
     </div>
-    <div class="side-navigator">
-      <p>
-        
-      </p>
-    </div>
-  </div>
-</template>
+  </template>
+  
 <script>
 import HistoryNavigatorMethods from './HistoryNavigatorMethods.vue';
 
