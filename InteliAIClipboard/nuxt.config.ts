dfabaa7468ad86d3addd5cf6666596f0ab577001
gewal0815 <gerrywalter@hotmail.de>
2023-02-21 14:28:40 +0100

    changes last

--- InteliAIClipboard/nuxt.config.ts
+++ InteliAIClipboard/nuxt.config.ts
@@ -1,10 +1,21 @@
 import { defineNuxtConfig } from 'nuxt/config'
 
-export default defineNuxtConfig({
-  modules: [
-   
-    
-  ]
+export default defineNuxtConfig ({
+
+  runtimeConfig: {
+    // The private keys which are only available server-side
+    apiSecret: '123',
+    // Keys within public are also exposed client-side
+    public: {
+      apiBase: '~/server/api/index.js'
+    },
+
+    serverMiddleware: [
+      '~/server/api/index.js'
+    ]
+  }
+
+
 })
 
 
