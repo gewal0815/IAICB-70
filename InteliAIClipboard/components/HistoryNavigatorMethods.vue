7d58330a60022e6ff8a463b3badf395ee1a1c098
gewal0815 <gerrywalter@hotmail.de>
2023-02-22 08:52:28 +0100

    Histroy

--- InteliAIClipboard/components/HistoryNavigatorMethods.vue
+++ InteliAIClipboard/components/HistoryNavigatorMethods.vue
@@ -6,31 +6,30 @@ export default {
     },
 
     addItem() {
-      if (!this.inputValue) {
-        const lastItem = this.history[this.history.length - 1];
-        if (lastItem && !lastItem.content) {
-          // The last item is empty, remove it
-          this.history.pop();
-        }
-        this.history.push({ id: Date.now(), content: '' });
-        this.inputValue = '';
-        this.showTextArea = true;
-      } else {
-        const newItem = { id: Date.now(), content: this.inputValue };
-        const existingItemIndex = this.history.findIndex(
-          (item) => item.id === newItem.id
-        );
-        if (existingItemIndex >= 0) {
-          // Update existing item
-          this.history[existingItemIndex] = newItem;
-        } else {
-          // Add new item
-          this.history.push(newItem);
-        }
+  if (!this.inputValue) {
+    const lastItem = this.history[this.history.length - 1];
+    if (lastItem && !lastItem.content) {
+      // The last item is empty, remove it
+      this.history.pop();
+    }
+    this.history.push({ id: Date.now(), content: '' });
+    this.inputValue = '';
+    this.showTextArea = true;
+  } else {
+    const existingItemIndex = this.history.findIndex(
+      (item) => item.content === this.inputValue
+    );
+    if (existingItemIndex >= 0) {
+      // Update existing item
+      this.history[existingItemIndex] = { id: Date.now(), content: this.inputValue };
+    } else {
+      // Add new item
+      this.history.push({ id: Date.now(), content: this.inputValue });
+    }
+    this.showTextArea = true;
+  }
+},
 
-        this.showTextArea = true;
-      }
-    },
 
     selectItem(item) {
       this.inputValue = item.content;
