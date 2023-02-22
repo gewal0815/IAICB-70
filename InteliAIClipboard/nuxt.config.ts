b5e7f1b6b1db66786325b196ad34f080dd426314
gewal0815 <gerrywalter@hotmail.de>
2023-02-22 15:07:27 +0100

    supabase-fix

--- InteliAIClipboard/nuxt.config.ts
+++ InteliAIClipboard/nuxt.config.ts
@@ -2,9 +2,15 @@ import { defineNuxtConfig } from 'nuxt/config';
 
 export default defineNuxtConfig({
 
+
+
     modules: [
-        "@nuxtjs/supabase"
-    ] 
+        '@nuxtjs/supabase'
+      ],
+      supabase: {
+        url: 'https://tthpipuymqkvbpzewjuw.supabase.co',
+        key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR0aHBpcHV5bXFrdmJwemV3anV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzY5NzgwNDcsImV4cCI6MTk5MjU1NDA0N30.1qGoCdgV-KOatTKOilvN9BLlW6Tk1TKyyCdk_Q7UXDA'
+      }
 
  
 });
