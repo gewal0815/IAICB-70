ac40a5100c7793d626a810130bea89052a67563a
gewal0815 <gerrywalter@hotmail.de>
2023-02-26 09:18:32 +0100

    fix the chromeExtension

--- InteliAIClipboard/background.js
+++ InteliAIClipboard/background.js
@@ -1,32 +1,44 @@
-chrome.contextMenus.create({
-    "id": "copy",
-    "title": "Copy Data",
-    "contexts": ["all"]
+// background.js
+
+const MENU_ITEM_ID = 'copy';
+
+chrome.contextMenus.remove(MENU_ITEM_ID, () => {
+  chrome.contextMenus.create({
+    id: MENU_ITEM_ID,
+    title: 'Copy Data',
+    contexts: ['all'],
   });
-  
-  chrome.contextMenus.onClicked.addListener(function(info, tab) {
-    if (info.menuItemId == "copy") {
-      chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-        const currentTab = tabs[0];
-        chrome.scripting.executeScript({
-          target: {tabId: currentTab.id},
-          files: ["contentScript.js"]
-        });
-      });
-    }
-  });
-  
-  
-  chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.action == "sendData") {
-      var data = request.data;
-      fetch("http://localhost:3000/api/data", {
-        method: "POST",
+});
+
+chrome.contextMenus.onClicked.addListener(async (info, tab) => {
+  if (info.menuItemId === MENU_ITEM_ID) {
+    const [currentTab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    chrome.scripting.executeScript({
+      target: { tabId: currentTab.id },
+      function: () => window.getSelection().toString(),
+    }, (results) => {
+      const data = results[0];
+      fetch('http://localhost:3000/api/data', {
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json"
+          'Content-Type': 'application/json',
         },
-        body: JSON.stringify({data: data})
+        body: JSON.stringify({ data }),
       });
-    }
-  });
-  
+    });
+  }
+});
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === 'copy') {
+    const { data } = message;
+    console.log("data"+data);
+    fetch('http://localhost:3000/api/data', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ data }),
+    });
+  }
+});
--- InteliAIClipboard/contentScript.js
+++ InteliAIClipboard/contentScript.js
@@ -1,7 +1,9 @@
-//contentScript.js
-chrome.runtime.sendMessage({
-    action: "copy",
-    data: window.getSelection().toString()
-  });
+// contentScript.js
 
-  
+alert('File test alert');
+
+document.addEventListener('copy', async () => {
+  const text = await navigator.clipboard.readText();
+  console.log("text"+text);
+  chrome.runtime.sendMessage({ action: 'copy', data: text });
+});
