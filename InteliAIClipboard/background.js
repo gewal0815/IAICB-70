f6d010352febcfb8df6f7e6d80b379cb20a3d33c
gewal0815 <gerrywalter@hotmail.de>
2023-02-27 08:32:23 +0100

    Working Chrome Extension v1.0

---
+++ InteliAIClipboard/background.js
@@ -0,0 +1,42 @@
+chrome.contextMenus.create({
+    id: "copy-data",
+    title: "Copy Data",
+    contexts: ["page", "selection"]
+  });
+  
+  chrome.contextMenus.onClicked.addListener(function(info, tab) {
+    if (info.menuItemId === "copy-data") {
+      chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        var tab = tabs[0];
+        chrome.scripting.executeScript({
+            target: {tabId: tab.id},
+            func: () => {
+              const selection = window.getSelection().toString();
+              const text = selection || document.title;
+              return text;
+            }
+          }, (result) => {
+            const text = result[0];
+          console.log("text:" + text);
+            const url = tab.url;
+          console.log("url:" + url);
+            const data = { url: url, text: text };
+            chrome.storage.local.set({ "copiedData": data }, () => {
+              chrome.contextMenus.update("copy-data", { title: "Copied!" });
+              setTimeout(() => {
+                chrome.contextMenus.update("copy-data", { title: "Copy Data" });
+              }, 5000);
+            });
+            chrome.permissions.request({permissions: ['clipboardWrite']}, (granted) => {
+              if (granted) {
+                navigator.clipboard.writeText(text);
+              }
+            });
+          });
+          
+      });
+    }
+  });
+
+
+  
