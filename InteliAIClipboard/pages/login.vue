f3d797a242634f10ee48813225160dc8a4abd5f4
gewal0815 <gerrywalter@hotmail.de>
2023-03-02 07:48:11 +0100

    SupabaseAuthLogin

---
+++ InteliAIClipboard/pages/login.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <button @click="login">Login with Google</button>
+  </div>
+</template>
+
+<script>
+import { createClient } from '@supabase/supabase-js';
+
+import { SUPABASEKEY, SUPABASEURL } from '../utils/key/key.vue';
+
+const supabase = createClient(SUPABASEURL, SUPABASEKEY);
+
+const login = async () => {
+  const { error } = supabase.auth.signInWithOAuth({
+    provider: 'google',
+  });
+  if (error) {
+    console.log('Supabase Auth Error' + error);
+  }
+};
+
+export default {
+  name: 'GoogleLogin',
+  methods: {
+    login,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
