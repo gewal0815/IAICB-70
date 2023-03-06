ec1e6587e42edf7182d304b809b02be25689ce23
gewal0815 <gerrywalter@hotmail.de>
2023-03-06 05:43:55 +0100

    css rsponsive changes

--- InteliAIClipboard/components/Content.vue
+++ InteliAIClipboard/components/Content.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="content" ref="content">
     <div class="card" v-for="(item, index) in history" :key="index">
-      <div class="card-grid">
-        <div class="card-header">{{ item.content }}</div>
-        <div class="card-body">{{ item.color }}</div>
+      <div class="card-inside">
+        <div class="card-content">{{ item.content }}</div>
+        <div>{{ item.color }}</div>
         <div class="card-buttons">
           <button class="business-btn" @click="deleteItem(index)">
             Delete
@@ -35,50 +35,39 @@ export default {
 </script>
 
 <style scoped>
-
 .content {
-    width: 99vw;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
+  max-width: 90%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  word-wrap: break-word;
+  word-break: break-word;
 }
 
 .card {
-  margin: 20px 22px 5px;
-  padding: 50px;
   border: var(--BorderSize) solid #ddd;
-  border-radius: 5px;
   background-color: #fff;
+  border-radius: 5px;
+  margin: 0 200px; /* added margin for large screens */
+  
+}
+
+.card-inside {
   display: flex;
+  flex-direction: column;
   justify-content: space-between;
-  align-items: center;
-  width:50%;
+  padding: 10px;
 }
 
-.card-grid {
-    display: grid;
-    grid-template-rows: repeat(1fr);
-    gap: 20px;
-}
-.card-grid > *:nth-child(1) {
-  grid-column: 2 / span 2;
-}
-
-.card-grid > *:nth-child(2) {
-  grid-column: 2 / span 1;
-}
-
-.card-grid > *:nth-child(3) {
-  grid-column: 4 / span 1;
-}
-
-.card-header {
-  font-weight: bold;
+.card-inside > div:first-child {
+  white-space: pre-wrap;
+  overflow-wrap: break-word;
 }
 
 .card-buttons {
   display: flex;
+  justify-content: flex-end;
+  align-items: center;
 }
 
 .business-btn {
@@ -86,8 +75,10 @@ export default {
   border-radius: 5px;
   background: transparent;
   color: var(--ButtonColor);
-  margin-left: 10px;
+  margin-bottom: 10px;
   padding: var(--ButtonPadding);
+  width: 150px;
+  flex-grow: 0;
 }
 
 .business-btn:hover {
@@ -98,4 +89,26 @@ export default {
 .business-btn:active {
   background-color: #0052cc;
 }
+
+@media (max-width: 768px) {
+  .content {
+    flex-direction: column;
+    align-items: center;
+    word-wrap: break-word;
+    word-break: break-word;
+  }
+
+  .card {
+    margin: 0 22px; /* added margin for small screens */
+  }
+
+  .card-inside {
+    padding: 20px; /* increased padding for small screens */
+  }
+
+  .business-btn {
+    width: 100%; /* full width for small screens */
+  }
+}
 </style>
+
