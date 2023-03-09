a0a92a71da8e66a674c48317baa2da37453d26ad
gewal0815 <gerrywalter@hotmail.de>
2023-03-09 08:07:10 +0100

    ssl certificate error

--- InteliAIClipboard/server/api/users/[id].ts
+++ InteliAIClipboard/server/api/users/[id].ts
@@ -1,62 +1,7 @@
 import { db } from '~~/server/db';
 
-import { sendError } from 'h3';
+
 
 export default defineEventHandler(async (e) => {
-  const findUserById = (tid: string) => {
-    let index = -1;
-    const user = db.users.find((u, i) => {
-      if (u.id == tid) {
-        index = i;
-        return true;
-      }
-      return false;
-    });
 
-    if (!user) {
-      const notfoundError = createError({
-        statusCode: 404,
-        statusMessage: 'User not found',
-      });
-      sendError(e, notfoundError);
-      return null;
-    }
-
-    return { user, index };
-  };
-
-  switch (e.node.req.method) {
-    case 'PUT': {
-      const { id } = e.context.params as { id: string };
-      const result = findUserById(id);
-      if (result === null) {
-        // handle the case where the user object is not found
-      }
-      const { user, index } = result!;
-
-      const updateUser = {
-        ...user,
-        online: !user.online,
-      };
-      db.users[index] = updateUser;
-      return updateUser;
-    }
-
-    case 'DELETE': {
-      const { id } = e.context.params as { id: string };
-
-      const result = findUserById(id);
-      if (result === null) {
-        // handle the case where the user object is not found
-      }
-      const { user, index } = result ?? { user: null, index: -1 };
-      if (user === null) {
-        // handle the case where the user object is not found
-      }
-      db.users.splice(index, 1);
-      return {
-        message: 'User deleted',
-      };
-    }
-  }
 });
--- InteliAIClipboard/server/api/users/index.ts
+++ InteliAIClipboard/server/api/users/index.ts
@@ -1,33 +1,8 @@
 import { db } from "../../db";
 import { v4 as uuid } from 'uuid';
-import { readBody, sendError} from "h3";
+
 
 export default defineEventHandler(async (e) => {
     
-    if (e.node.req.method == "GET") {
-        return db.users;
-    }
 
-    if (e.node.req.method == "POST") {
-        const body = await readBody(e);
-        if (!body.username) {
-            const badRequestErr = createError({
-                statusCode: 400,
-                statusMessage: "Username property not found",
-
-            });
-
-            sendError(e, badRequestErr);
-
-        }
-        const newUser = {
-            id: uuid(),
-            item: '',
-            online: true,
-        };
-
-        db.users.push(newUser);
-        return newUser;
-
-    }
 });
