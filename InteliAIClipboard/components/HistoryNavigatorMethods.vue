b6b9b222d2d773b1a82f7461f5db92a426401ce8
gewal0815 <gerrywalter@hotmail.de>
2023-03-10 08:44:47 +0100

    Date fixes , and history Array

--- InteliAIClipboard/components/HistoryNavigatorMethods.vue
+++ InteliAIClipboard/components/HistoryNavigatorMethods.vue
@@ -7,11 +7,12 @@ export default {
     addItem() {
       if (!this.inputValue) {
         const lastItem = this.history[this.history.length - 1];
+        const lastItemId = this.history[this.history.length - 1].id;
         if (lastItem && !lastItem.content) {
           // The last item is empty, remove it
           this.history.pop();
         }
-        this.history.push({ id: Date.now(), content: '' , color: 'blue' });
+        this.history.push({ id: lastItemId +1, content: '', color: 'blue' , created_at: Date.now() });
         this.inputValue = '';
         this.showTextArea = true;
       } else {
@@ -22,26 +23,45 @@ export default {
 
         // Add the item to the history with the appropriate color
         if (isUrl) {
+          const lastItemId = this.history[this.history.length - 1].id;
+
           this.history.push({
-            id: Date.now(),
+            id: lastItemId +1,
             content: this.inputValue,
             color: 'green',
-            created_at: Date.now()
+            created_at: Date.now(),
           });
         } else {
           const existingItemIndex = this.history.findIndex(
             (item) => item.content === this.inputValue
           );
           if (existingItemIndex >= 0) {
+            const lastItemId = this.history[this.history.length - 1].id;
             // Update existing item
             this.history[existingItemIndex] = {
-              id: Date.now(),
+              id: lastItemId +1,
               content: this.inputValue,
-              color: 'blue'
+              color: 'blue',
             };
           } else {
             // Add new item
-            this.history.push({ id: Date.now(), content: this.inputValue, color: 'blue', created_at: Date.now() });
+            if (this.history.length) {
+              const lastItemId = this.history[this.history.length - 1].id;
+              console.log('lastItem:->' + lastItemId);
+              this.history.push({
+                id: lastItemId + 1,
+                content: this.inputValue,
+                color: 'blue',
+                created_at: Date.now(),
+              });
+            } else {
+              this.history.push({
+                id:  1,
+                content: 'test',
+                color: 'blue',
+                created_at: Date.now(),
+              }); 
+            }
           }
         }
         this.showTextArea = true;
