9ace281b51ed4f40297b5b8c0db0c5193258cb73
gewal0815 <gerrywalter@hotmail.de>
2023-03-13 11:40:12 +0100

    fixes

--- InteliAIClipboard/nuxt.config.ts
+++ InteliAIClipboard/nuxt.config.ts
@@ -3,9 +3,7 @@ import { defineNuxtConfig } from 'nuxt/config';
 export default defineNuxtConfig({
 
   
-    plugins: [
-        { src: '~/plugins/supabase.js', ssr: false }
-      ],
+
 
     modules: [
        "@nuxtjs/supabase"
